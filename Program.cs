2c68a0321d2d4a52bf5b650066644a19ee7c4e78
EmilSchnoetzingerV <emil4000@gmx.at>
2025-09-16 12:07:20 +0200

    brunch des Spaltissimuss

--- Program.cs
+++ Program.cs
@@ -7,3 +7,4 @@ Console.WriteLine("add_something #7");
 Console.WriteLine("add_something #3");
 
 Console.WriteLine("Test nachdem Spalt die WELT gerettet hat.");
+Console.WriteLine("Mit Spaltigem Brunch");
