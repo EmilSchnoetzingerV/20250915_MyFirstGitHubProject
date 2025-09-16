69cf81b96d1ec586e501696fb432a6c4f58b0a6c
EmilSchnoetzingerV <emil4000@gmx.at>
2025-09-16 12:44:49 +0200

    joka

--- Program.cs
+++ Program.cs
@@ -8,3 +8,4 @@ Console.WriteLine("add_something #3");
 
 Console.WriteLine("Test nachdem Spalt die WELT gerettet hat.");
 Console.WriteLine("Mit Spaltigem Brunch");
+Console.WriteLine("Beim pcs hinzufügen");
