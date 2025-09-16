635de1ed7179c61ef4bc6c7afb8e07c517e915d4
EmilSchnoetzingerV <emil4000@gmx.at>
2025-09-16 12:53:05 +0200

    jetzt funktioniert es

--- Program.cs
+++ Program.cs
@@ -1,12 +1,2 @@
 ﻿
-Console.WriteLine("Version #1");
-
-Console.WriteLine("add_something #9");
-Console.WriteLine("add_something #7");
-
-Console.WriteLine("add_something #3");
-
-Console.WriteLine("Test nachdem Spalt die WELT gerettet hat.");
-Console.WriteLine("Mit Spaltigem Brunch");
-Console.WriteLine("Beim pcs hinzufügen");
-Console.WriteLine("123");
+Console.WriteLine("jetzt funktioniert es");
